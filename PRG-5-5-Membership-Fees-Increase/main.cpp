4ee5228399d7dd1896827a58b2fd2298468b13a1
Keith Smith <aeiouandsometimesy@me.com>
2017-10-07 14:47:21 -0400

    Updated and fixed missing semicolon

--- PRG-5-5-Membership-Fees-Increase/main.cpp
+++ PRG-5-5-Membership-Fees-Increase/main.cpp
@@ -5,11 +5,32 @@
 //  Created by Keith Smith on 10/7/17.
 //  Copyright © 2017 Keith Smith. All rights reserved.
 //
+//  A country club, which currently charges $2,500 per year for membershiop, has announced
+//  it will incrase its membership fee by 4 percent each year for the next 6 years. Write a
+//  program that uses a loop to display the projected rates for the next 6 years.
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main()
+{
+    int intYear = 2017,
+        intYearAdjusted = intYear + 6;
+    
+    float fltMembershipFee = 2500.00f,
+          fltMemberShipFeeProjected;
+    
+    const float FLT_MEMBERSHIP_INCREASE = 1.04f;
+    
+    do
+    {
+        fltMemberShipFeeProjected = fltMembershipFee * FLT_MEMBERSHIP_INCREASE;
+        cout << fltMemberShipFeeProjected << endl;
+        intYear++;
+    } while(intYear <= intYearAdjusted);
+        
     return 0;
 }
+
+
